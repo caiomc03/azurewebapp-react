7e1c7cfb75855eb05967d7e6d6a679884a22eedf
caiomc03 <caiomc03@gmail.com>
2023-07-04 23:44:20 -0300

    add pog

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const pogImg = 'https://caiomc03bucket.blob.core.windows.net/container2/pog.png'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pogImg} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
